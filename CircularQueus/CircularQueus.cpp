cd5a6b9e81beb94e822b63c5f832f608fb1b920d
agilimam <agilimamalhafizi04@gmail.com>
2023-06-23 20:46:28 +0700

    hasil dari pilihan user akan di simpan di ch

--- CircularQueus/CircularQueus.cpp
+++ CircularQueus/CircularQueus.cpp
@@ -112,6 +112,10 @@ int main() {
 			cout << "1. Implement insert operation" << endl;
 			cout << "2. Implement delete operation" << endl;
 			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4) : ";
+			cin >> ch;
+			cout << endl;
 		}
 	}
 }
