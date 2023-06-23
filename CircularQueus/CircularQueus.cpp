8b72e925fbdaffc636dfb3f03a8046601ff6bac7
agilimam <agilimamalhafizi04@gmail.com>
2023-06-23 20:41:36 +0700

    mendeskripsikan iterasi dari awal array hingga REAR

--- CircularQueus/CircularQueus.cpp
+++ CircularQueus/CircularQueus.cpp
@@ -70,7 +70,7 @@ public:
 			cout << "Queue is empty\n";
 			return;
 		}
-		
+
 		cout << "\nElement in the queue are...\n";
 
 		//jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
@@ -81,7 +81,23 @@ public:
 			}
 			cout << endl;
 		}
- 	
+		else {
+			//jika FRONT_position > REAR_position,iterasi dari FRONT hingga akhir aaray
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+
+			//iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+
 	}
 
-}
+};
